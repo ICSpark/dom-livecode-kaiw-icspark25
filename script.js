6ff31da4d317139bc7a4b567ec7639fc0f537441
kaiw-icspark25 <chinow898@gmail.com>
2025-03-26 23:14:16 +0000

    Kai

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('main-title').style.textAlign='center';
+document.getElementById('description').style.textAlign='center';
+document.getElementById('main-title').style.textAlign='center';
+
+const pizzaList
